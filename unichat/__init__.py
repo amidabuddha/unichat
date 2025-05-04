8465f02acf772e80173731ef973cbc38ba5595f3
Stefan <shear.0-corned@icloud.com>
2025-05-04 11:40:34 +0300

    Revert export

--- unichat/__init__.py
+++ unichat/__init__.py
@@ -1,7 +1,2 @@
 from .unichat import UnifiedChatApi
 from .models import MODELS_LIST
-from .api_helper import _ApiHelper
-
-api_helper_function = _ApiHelper().transform_tools
-
-__all__ = ["api_helper_function"]
